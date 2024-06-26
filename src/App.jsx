e0e163385d367bf7c85a6880f277be97cb4d0708
David Martin <dmartin5150@icloud.com>
2024-06-25 20:55:24 -0500

    fixed import

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import { Toaster } from 'react-hot-toast';
 import Booking from './pages/Booking';
-import Checkin from './pages/checkin';
+import Checkin from './pages/Checkin';
 import ProtectedRoute from './ui/ProtectedRoute';
 import { DarkModeProvider } from './context/DarkModeContext';
 
